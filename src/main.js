fadc0e071e97afe6a30c7bcb1a02cf7feb749d56
Thom Bruce <thom@thombruce.com>
2021-09-20 23:35:55 +0100

    vue add @thombruce/tnt

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import TNT from '@thombruce/tnt'
+
+import '@thombruce/tnt/dist/tnt.css'
+import '@thombruce/tnt/src/assets/index.css'
+
+Vue.use(TNT)
 
 Vue.config.productionTip = false
 
