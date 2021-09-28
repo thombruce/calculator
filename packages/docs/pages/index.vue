d70651dadc79ebe4baa05443a8162bfbb000b783
Thom Bruce <thom@thombruce.com>
2021-09-28 06:07:59 +0100

    Add docs site

--- packages/docs/pages/index.vue
+++ packages/docs/pages/index.vue
@@ -1,7 +1,16 @@
-<template>
-  <Tutorial/>
+<template lang='pug'>
+div.max-w-prose.h-full.flex.content-center.justify-center.m-auto
+  .card.bordered.w-full.m-auto
+    .card-body
+      Calculator
 </template>
 
 <script>
-export default {}
+import Calculator from '@thombruce/calculator/src/components/Calculator.vue'
+
+export default {
+  components: {
+    Calculator
+  }
+}
 </script>
